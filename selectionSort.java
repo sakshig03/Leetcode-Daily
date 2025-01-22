073a641d28db8b74df7742ef45cf9a051ef4bcff
Sakshi Gaikwad <123818553+sakshig03@users.noreply.github.com>
2025-01-22 11:12:47 +0530

    Selection Sort

---
+++ selectionSort.java
@@ -0,0 +1,30 @@
+public class selectionSort {
+    public static void selection(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < n; j++) { // Fixed condition
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) { // Fixed condition and print statement
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        selection(arr);
+        printArr(arr);
+    }
+}
